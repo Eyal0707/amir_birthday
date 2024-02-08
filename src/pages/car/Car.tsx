6c4385f6af90869c38e24251b8817acce609689a
Eyal Florentin <eyal0707@gmail.com>
2024-02-08 02:15:26 +0200

    finished second page and road game

--- src/pages/car/Car.tsx
+++ src/pages/car/Car.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import "./Car.css";
+import useRiddle from '../../hooks/useRiddle';
 
 const Car = () => {
+  const [answer, onAnswerChange] = useRiddle("raptor", "/road")
   return (
     <div className='car'>
 
+
+      <audio id='music' autoPlay loop>
+        <source src='./assets/music/shake_it_off.mp3' type='audio/mpeg' />
+      </audio>
+      <div className="main">
+        <div className="riddle">
+          <div className="question">
+            <h1>אני בחוץ!</h1>
+            <p>בוא יוצאים מזמן לא התראנו! הגיע הזמן לעוד יציאת אמיר ואייל קלאסית! אנחנו הולכים לסרט ואחרי אפשר ללכת לפארק ההוא עם הציפורים שאהבת.</p>
+            <h2>חידה #2</h2>
+            <h3>למה הציפורים האלה דומות כשהן רצות?</h3>
+          </div>
+          <input className='answer' value={answer} onChange={onAnswerChange} />
+        </div>
+        <img className='car_image' src="./assets/pictures/car.png" />
+        <img className='car-face' src="./assets/pictures/eyal_car_face.png" />
+      </div>
     </div>
   )
 }
--- src/pages/road/Road.tsx
+++ src/pages/road/Road.tsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./Road.css"
 
 const Road = () => {
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [gameStartCountdown, setGameStartCountdown] = useState(2);
+
+  const [targetCount, setTargetCount] = useState(1);
+
+  const [isLost, setIsLost] = useState(false);
+  const [isWin, setIsWin] = useState(false);
+
+  const [currentTimeOut, setCurrentTimeOut] = useState<NodeJS.Timeout>();
+
+
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (gameStartCountdown > 0) {
+        setGameStartCountdown(prev => prev - 1);
+      } else {
+        clearInterval(intervalId);
+        startGame()
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [gameStartCountdown]);
+
+
+  function targetTimeOut(targetId: number) {
+    console.log(targetCount, targetId)
+    if (targetId >= targetCount) {
+      setIsLost(true);
+    }
+  }
+  function startGame() {
+    setIsGameOn(true);
+    setCurrentTimeOut(setTimeout(() => targetTimeOut(targetCount), 4000));
+  }
+
+  function onTargetClick() {
+    clearTimeout(currentTimeOut);
+    if (targetCount > 20) {
+      setIsWin(true);
+    } else {
+      setCurrentTimeOut(setTimeout(() => targetTimeOut(targetCount + 1), 4000))
+    }
+    setTargetCount(targetCount + 1);
+  }
+
+
   return (
-    <div>Road</div>
+    <div className='road'>
+      <audio id='music' autoPlay loop>
+        <source src='./assets/music/tokio_drift.mp3' type='audio/mpeg' />
+      </audio>
+      {
+        isGameOn
+          ? <div className="game">
+            {
+              isLost
+                ? <div className="lost">LOST</div>
+                : <>
+                  {
+                    isWin
+                      ? <div className="win"></div>
+                      : <img src="./assets/pictures/traffic_light.jpg" className={`target count_${targetCount}`} onClick={onTargetClick} />
+                  }
+                </>
+            }
+          </div>
+          : <div className="game-explain">
+            <h1>אנחנו מאחרים לסרט!</h1>
+            <p>תלחץ על כל הרמזורים כדי שיהיו ירוקים! אני לא עוצר בהם!!</p>
+            <p>חלק מהשיבוטים איטיים או משהו לא יודע...</p>
+            <h2>{gameStartCountdown}</h2>
+          </div>
+      }
+    </div>
   )
 }
 
